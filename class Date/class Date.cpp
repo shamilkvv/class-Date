c4f1beff72a197652e9bcb3bf150839a7ef066ea
shamilkvv <shamiltv803@gmail.com>
2023-12-08 18:50:24 +0400

    add Normalize function

--- class Date/class Date.cpp
+++ class Date/class Date.cpp
@@ -22,18 +22,60 @@ private:
         return (month == 2 && IsLeapYear(year)) ? 29 : daysInMonth[month];
     }
 
+    // корректное представление даты
+
+    void Normalize()
+    {
+        if (month != 12)
+        {
+            year += month / 12;
+            month %= 12;
+        }
+
+        if (month < 1)
+        {
+            month = 12 + month;
+            year--;
+        }
+
+        while (day > DaysInMonth(year, month))
+        {
+            day -= DaysInMonth(year, month);
+            month++;
+
+            if (month > 12)
+            {
+                year++;
+                month = 1;
+            }
+        }
+
+        while (day <= 0)
+        {
+            month--;
+
+            if (month < 1)
+            {
+                year--;
+                month = 12;
+            }
+
+            day += DaysInMonth(year, month);
+        }
+    }
+
 public:
 
     // конструкторы
 
     Date(int day, int month, int year) : day(day), month(month), year(year)
     {
-
+        Normalize();
     }
 
     Date(int day) : day(day), month(0), year(1)
     {
-
+        Normalize();
     }
 
     int GetYear() const
@@ -50,7 +92,6 @@ public:
     {
         return day;
     }
-
 };
 
 int main()
